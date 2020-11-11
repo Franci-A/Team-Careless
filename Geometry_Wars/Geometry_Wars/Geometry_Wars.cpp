5afcd0e25cb9f6e10ef49ae3e32c3bb8dd10e0aa
MrEsport <lucasespositonc@gmail.com>
2020-11-11 15:16:32 +0100

    .sln fixed, files restored

--- Geometry_Wars/Geometry_Wars/Geometry_Wars.cpp
+++ Geometry_Wars/Geometry_Wars/Geometry_Wars.cpp
@@ -1,13 +1,18 @@
 // Geometry_Wars.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include "bullet.cpp"
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+extern TIME T;
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
 	// Initialise everything below
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Time : " << T.time << ".\n";
+	}
 	// Game loop
 	while (window.isOpen()) {
 		sf::Event event;
